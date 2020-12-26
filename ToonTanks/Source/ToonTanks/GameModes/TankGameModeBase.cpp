68a9e2ace065068f4b9f18f09f1eb982f597b6b9
tsava <sava.taras@gmail.com>
2020-12-26 12:17:00 +0100

    Added health reduction logic

--- ToonTanks/Source/ToonTanks/GameModes/TankGameModeBase.cpp
+++ ToonTanks/Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -1,5 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "TankGameModeBase.h"
+
+void ATankGameModeBase::BeginPlay()
+{
+}
+
+void ATankGameModeBase::ActorDied(AActor *DeadActor)
+{
+    UE_LOG(LogTemp, Error, TEXT("A Pawn Died!"));
+}
+
+void ATankGameModeBase::HandleGameStart()
+{
+}
+
+void ATankGameModeBase::HandleGameOver(bool PlayerWon)
+{
+}
 
--- ToonTanks/Source/ToonTanks/GameModes/TankGameModeBase.h
+++ ToonTanks/Source/ToonTanks/GameModes/TankGameModeBase.h
@@ -13,5 +13,20 @@ UCLASS()
 class TOONTANKS_API ATankGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+private:
+	void HandleGameStart();
+	void HandleGameOver(bool PlayerWon);
+
+public:
+	void ActorDied(AActor *DeadActor);
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void GameStart();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void GameOver(bool PlayerWon);
 };
